ffdc0ee72f9f53eacfd7b91310c86f541cfdb44e
anthonyym <ym.hades@gmail.com>
2023-02-01 00:04:24 -0500

    add make_json_config, progress_dialog, SuccessWindow that opens output directory

--- QualtricsToESS.py
+++ QualtricsToESS.py
@@ -4,8 +4,6 @@ import pandas as pd
 import json
 import platform
 from collections import OrderedDict
-import subprocess
-import webbrowser
 
 def ballot_list_to_excel(ballots, file_path, file_name):
     """Takes in the ballots, which are a list of lists, and writes to an excel sheet in election ESS CVR format"""
@@ -21,16 +19,20 @@ def ballot_list_to_excel(ballots, file_path, file_name):
     df.insert(1, 'Precinct', file_name)
     df.insert(2, 'Ballot Style', 'Qualtrics')
 
-    writer = pd.ExcelWriter(file_path + '.xlsx')
+    writer = pd.ExcelWriter(file_path)
     df.to_excel(writer, sheet_name='ballots', index=False, header=True)
     writer.save()
 
-def convert_to_ballots(df):
+def convert_to_ballots(df, progress_dialog):
     """Convert the dataframe to ballots (a list of lists)"""
+    # the first row index containing ballots
+    ballot_idx_start = 2
+    num_ballots_total = len((df[ballot_idx_start:]).index)
+
     ballots = []*len(df)
 
     # loop through each row (ballot)
-    for row_idx, row_contents in df[2:].iterrows():
+    for row_idx, row_contents in df[ballot_idx_start:].iterrows():
         ballot = [] # list of candidates
         result_dict = OrderedDict() # key: cell contents (ranking), value: candidate given that ranking
 
@@ -39,8 +41,6 @@ def convert_to_ballots(df):
             if (row_contents[col_idx] != ''):
                 result_dict[row_contents[col_idx]] = (str(df.iloc[0, col_idx]))
 
-        # result_dict = {key: value for key, value in result_dict.items() if key==key}
-
         # sort result_dict by the key (ranking)
         # e.g. ballot = {1: Option 3, 2: Option 4, 3: Option 2, 4: Option 1}
         ballot = OrderedDict(sorted(result_dict.items()))
@@ -48,9 +48,61 @@ def convert_to_ballots(df):
         # take the values (the candidates)
         ballot = list(ballot.values())
         ballots.append(ballot)
+
+        ballot_count = row_idx - ballot_idx_start + 1
+        # want to reset progress_dialog for each election
+        # so subtract 1 in order to not reach max, else ProgressDialog closes
+        progress_dialog.Update(int((ballot_count / num_ballots_total) * 100) - 1)
     return(ballots)
 
-def qualtrics_to_ess(input_csv):
+def make_json_config(input_csv, contest_name, candidates, output_folder):
+    """Creates JSON configuration file and writes it to output_folder"""
+    candidate_map_list = []*len(candidates)
+    for candidate in candidates:
+        candidate_map_list.append({
+            "name": candidate,
+            "code": "",
+            "excluded": False
+        })
+
+    output_dict = {
+        "tabulatorVersion": "1.3.0",
+        "outputSettings": {
+            "contestName": contest_name,
+            "outputDirectory": "RCTab_Output\\" + contest_name,
+            "tabulateByPrecinct": False,
+            "generateCdfJson": False
+        },
+        "cvrFileSources": [{
+            "provider": "ess",
+            "filePath": input_csv,
+            "contestId": "",
+            "firstVoteColumnIndex": "4",
+            "firstVoteRowIndex": "2",
+            "idColumnIndex": "1",
+            "precinctColumnIndex": "2",
+            "overvoteDelimiter": "",
+            "overvoteLabel": "overvote",
+            "undervoteLabel": "undervote",
+            "undeclaredWriteInLabel": "",
+            "treatBlankAsUndeclaredWriteIn": False
+        }],
+        "candidates": candidate_map_list,
+        "rules": {
+            "tiebreakMode": "previousRoundCountsThenRandom",
+            "overvoteRule": "exhaustImmediately",
+            "winnerElectionMode": "singleWinnerMajority",
+            "numberOfWinners": "1",
+            "decimalPlacesForVoteArithmetic": "4",
+            "maxSkippedRanksAllowed": "unlimited",
+            "maxRankingsAllowed": "max",
+            "randomSeed": "1234"
+        }
+    }
+    with open(output_folder + '\\' + contest_name + '.json', 'w', encoding='utf-8') as file:
+        json.dump(output_dict, file, indent=4)
+
+def qualtrics_to_ess(input_csv, progress_dialog):
     '''Converts the input_csv into ESS/CVR format Excel files. Returns the folder location of the Excel files'''
     df = pd.read_csv(input_csv)
     # ignore row if all null
@@ -60,34 +112,64 @@ def qualtrics_to_ess(input_csv):
 
     # get indices of question columns (column name starts with Q and contains underscore)
     qcol_idx_list = []
-    json_row_num = 1 # row in df containing json values (e.g. {"ImportId": QID1_1})
+    # row in df containing json values (e.g. {"ImportId": QID1_1}) - note Pandas take first row as headers
+    json_row_num = 1
     # check the third row containing json values 
     json_row = df.iloc[json_row_num].values
-    for idx in range(len(json_row)):
+
+    for idx in range(len(json_row)):        
         import_id_value = json.loads(json_row[idx])["ImportId"]
         if import_id_value.startswith("Q") and "_" in import_id_value and "_TEXT" not in import_id_value:
             qcol_idx_list.append(idx)
             # rename json cell to keep only question ID (Q1D1_1 to become Q1D1)
             df.iloc[json_row_num, idx] = import_id_value.split("_")[0]
 
-    df = df.iloc[:,qcol_idx_list]
+    # keep only the question columns
+    df = df.iloc[:,qcol_idx_list]       
+
+    # row containing the candidates
+    candidate_row_num = 0
+    # exported Qualtrics CSV is in the format "[question text] - [candidate name]"
+    # keep only the candidate name
+    for idx in range(len(df.columns)):
+        df.iloc[candidate_row_num, idx] = " - ".join(df.iloc[candidate_row_num, idx].split(" - ")[1:])
+    
 
     # sorted set of unique column names
     unique_json_col_names = sorted(set(df.iloc[json_row_num]))
     for col_name in unique_json_col_names:
+        # columns for this question, e.g. Q1_1, Q1_2, Q1_3
         q_col_names = df.iloc[json_row_num][df.iloc[json_row_num] == col_name].index
         # get only columns with this value
         sub_df = pd.DataFrame(df.loc[:,q_col_names])
         # get what's to the left of underscore in the first question cell of row
-        question_name = q_col_names[0].split('_')[0]
+        election_name = q_col_names[0].split('_')[0]
         
         filename = input_csv.split('\\')[-1].replace('.csv', '')
         output_folder = os.getcwd() + '\\' + 'converted' + '\\' + filename
-        filepath = output_folder + '\\' + filename + '_' + question_name
+        filepath = output_folder + '\\' + filename + '_' + election_name + '.xlsx'
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
-        ballot_list_to_excel(convert_to_ballots(sub_df), filepath, filename + '_' + question_name)
+        
+        candidates = list(sub_df.iloc[0])
+        make_json_config(filepath, filename + '_' + election_name, candidates, output_folder)
+        progress_dialog.Update(0, "Reading ballots for election: " + election_name)
+        ballot_list_to_excel(convert_to_ballots(sub_df, progress_dialog), filepath, filename + '_' + election_name)
     return(output_folder)
+
+class SuccessWindow(wx.Dialog):
+    def __init__(self, output_dir, *args, **kwds):
+        wx.Dialog.__init__(self, *args, **kwds)
+        self.output_dir = output_dir
+
+    def show_ui(self):
+        self.button_candidate_file_browse = wx.Button(self, wx.ID_ANY, "Browse...")
+        self.Bind(wx.EVT_BUTTON, self.open_dir, self.button_candidate_file_browse)
+
+    def open_dir(self):
+        if platform.system() == "Windows":
+            os.startfile(self.output_dir)
+
 
 class WindowNew(wx.Dialog):
     def __init__(self, *args, **kwds):
@@ -95,6 +177,7 @@ class WindowNew(wx.Dialog):
 
         self.label_candidate_file = None
         self.button_create = None
+        self.max_progress_dialog_value = 100
 
         self.show_ui()
     
@@ -105,7 +188,7 @@ class WindowNew(wx.Dialog):
         self.Bind(wx.EVT_BUTTON, self.ui_browse_candidate_file, self.button_candidate_file_browse)
 
         self.button_create = wx.Button(self, wx.ID_ANY, "Convert")
-        self.Bind(wx.EVT_BUTTON, self.ui_create, self.button_create)
+        self.Bind(wx.EVT_BUTTON, self.ui_convert, self.button_create)
         self.button_create.Enable(False)
 
         self.sizer_main = wx.FlexGridSizer(2, 1, 5, 0)
@@ -135,23 +218,16 @@ class WindowNew(wx.Dialog):
         self.candidate_file = candidate_file
         self.text_ctrl_candidate_file.SetValue(self.candidate_file)
 
-    def explore(self, path):
-            FILEBROWSER_PATH = os.path.join(os.getenv('WINDIR'), 'explorer.exe')
-            # explorer would choke on forward slashes
-            path = os.path.normpath(path)
-
-            if os.path.isdir(path):
-                subprocess.run([FILEBROWSER_PATH, path])
-            elif os.path.isfile(path):
-                subprocess.run([FILEBROWSER_PATH, '/select,', path])
-
-    def ui_create(self, event):
-        output_dir = qualtrics_to_ess(self.candidate_file)
+    def ui_convert(self, event):
+        progress_dialog = wx.ProgressDialog("Processing Ballots", "", maximum=self.max_progress_dialog_value, parent=self, style=wx.PD_APP_MODAL | wx.PD_AUTO_HIDE | wx.PD_ELAPSED_TIME | wx.PD_ESTIMATED_TIME | wx.PD_REMAINING_TIME)
+        progress_dialog.Fit()
+        output_dir = qualtrics_to_ess(self.candidate_file, progress_dialog)
+        progress_dialog.Update(self.max_progress_dialog_value)
+        progress_dialog.Destroy()
         
-        wx.MessageDialog(self, "Conversion successful. \n\nOutputted to: " + output_dir, style=wx.OK | wx.OK | wx.CENTRE).ShowModal()
-        
-        if platform.system() == "Windows":
-            os.startfile(output_dir)
+        if wx.MessageDialog(self, "Conversion successful. \n\nOutputted to: \n\n" + output_dir, caption="Open outuptted files?", style=wx.YES_NO | wx.YES_DEFAULT | wx.ICON_QUESTION).ShowModal() == wx.ID_YES:
+            if platform.system() == "Windows":
+                os.startfile(output_dir)
 
     def ui_check_complete(self):
         self.button_create.Enable(not not (self.candidate_file))
